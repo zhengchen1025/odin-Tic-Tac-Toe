a031c7b2a4fdea6e3ab75bfdf83bf94e18624b1e
ZhengChen <zhengchen1025@gmail.com>
2024-07-21 23:18:49 +0800

    ready to add Mark feature

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const cells = document.querySelectorAll('td[data-row][data-col]');
+
+const Gameboard = {
+    '0,0': null, '0,1': null, '0,2': null,
+    '1,0': null, '1,1': null, '1,2': null, 
+    '2,0': null, '2,1': null, '2,2': null
+  };
+
+  cells.forEach(cell => {
+
+    const row = parseInt(cell.dataset.row);
+    const col = parseInt(cell.dataset.col);
+  
+    cell.addEventListener('click', () => {
+      console.log(`Clicked cell at row ${row}, column ${col}`);
+      
+    });
+  });
