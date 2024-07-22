5685b3df77377b7fc97b3d4b670b77a6c568565b
ZhengChen <zhengchen1025@gmail.com>
2024-07-22 14:19:43 +0800

    can detect winner now

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-const cells = document.querySelectorAll("td[data-row][data-col]");
-const info = document.querySelector('.info');
+const tableCells = document.querySelectorAll("td[data-row][data-col]");
+const info = document.querySelector(".info");
+let isWin = false;
 
 const Gameboard = {
   "0,0": null,
@@ -14,38 +15,73 @@ const Gameboard = {
 };
 
 const checkTurns = {
-    turn: 1,
-    takeTurns() {
-        this.turn *= -1;
-        return this.turn;
-    }
-}
+  turn: 1,
+  takeTurns() {
+    this.turn *= -1;
+    return this.turn;
+  },
+};
 
-
-
-cells.forEach((cell) => {
+tableCells.forEach((cell) => {
   const row = parseInt(cell.dataset.row);
   const col = parseInt(cell.dataset.col);
-  
+
   cell.addEventListener("click", () => {
-    
     console.log(`Clicked cell at row ${row}, column ${col}`);
 
     if (checkTurns.takeTurns() === -1) {
-         cell.textContent = 'x'
-        checkTurns.takeTurns();
-        console.log(checkTurns.takeTurns())
-    }
-    else {
-         cell.textContent = 'o'
-        checkTurns.takeTurns();
-        console.log(checkTurns.takeTurns())
+      cell.textContent = "x";
+      info.textContent = "Player 2's turn";
+      isWin = checkWin(tableCells);
+      console.log(isWin);
+
+    } else {
+      cell.textContent = "o";
+      info.textContent = "Player 1's turn";
+      isWin = checkWin(tableCells);
+      console.log(isWin);
+
     }
   });
 });
 
-console.log(checkTurns.takeTurns());
-console.log(checkTurns.takeTurns());
-console.log(checkTurns.takeTurns());
-console.log(checkTurns.takeTurns());
-console.log(checkTurns.takeTurns() === -1)
+function checkWin(cells) {
+  function allEqual(array) {
+    return array.every((textContent) => textContent === array[0] && textContent !== "");
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      allEqual([
+        cells[i * 3].textContent,
+        cells[i * 3 + 1].textContent,
+        cells[i * 3 + 2].textContent,
+      ])
+    ) {
+      return true;
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    if (allEqual([
+      cells[i].textContent,
+      cells[i + 3].textContent,
+      cells[i + 6].textContent
+    ])) {
+      return true;
+    }
+  }
+  if (allEqual([cells[0].textContent, cells[4].textContent, cells[8].textContent]) || allEqual([cells[2].textContent, cells[4].textContent, cells[6].textContent])) {
+    return true;
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
